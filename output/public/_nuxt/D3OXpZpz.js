ab22bef3743f510fad9c82735d8c21de64d5299a
Pedro Martins <pedro.olmartins@outlook.com>
2025-01-08 23:43:27 -0300

    remove arquivos desnecessários do diretório de saída

--- output/public/_nuxt/D3OXpZpz.js
+++
@@ -1,1 +0,0 @@
-import{b2 as t}from"./Br6iN-mP.js";const r=t((e,o)=>{});export{r as default};
--- output/public/_nuxt/KO9BYWvt.js
+++
@@ -1,1 +0,0 @@
-import{h as e,ak as o,o as n,c as t}from"./Br6iN-mP.js";const _=e({__name:"login",setup(a){return o(),(s,c)=>(n(),t("div",null," Login "))}});export{_ as default};
